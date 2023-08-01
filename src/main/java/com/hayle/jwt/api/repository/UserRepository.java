bd52e98942319dc53c863a7037d725f8667ae90a
HaileMelekot Gebremedhin <HaileMelekotG@HIBRETBANK.COM>
2023-08-01 15:52:21 +0300

    hayle commit

---
+++ src/main/java/com/hayle/jwt/api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hayle.jwt.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hayle.jwt.api.entity.User;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUserName(String username);
+}
